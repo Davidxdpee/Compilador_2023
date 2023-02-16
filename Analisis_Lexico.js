ffbb4893406b8fb28e108b005d3ec755392cdd69
Davidxdpee <124410847+Davidxdpee@users.noreply.github.com>
2023-02-15 18:23:33 -0600

    Update Analisis_Lexico.js

--- Analisis_Lexico.js
+++ Analisis_Lexico.js
@@ -1,83 +1,18 @@
-// const compilador = document.getElementById('boton');
-// const textarea = document.getElementById("textarea");
-// const importbutton = document.getElementById("import")
-// let datatype = "Hola";
-
-// importbutton.addEventListener("click", () => {
-
-
-
-// })
-
-
-// compilador.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   const declarations = obtenerDeclaraciones()
-//   const arraynorepeat = removewords(declarations);
-  
-//   PrintTable(arraynorepeat);
-// })
-
-// function PrintTable(table) {
-//   var fill = document.getElementById("tabla");
-//   var data = "";
-
-//   console.log(table)
-//   for (var i = 0; i < table.length; i++) {
-//     data += "<tr><td>" + table[i] + "</td><td>" + datatype;
-
-//   }
-//   fill.innerHTML = data;
-// }
-
-// function obtenerDeclaraciones(){
-//   const lines = textarea.value.split("\n");
-//   let declarations = [];
-  
-//   for (let line of lines) {
-//     let lineWords = line.split(" ");
-    
-//     if(lineWords[0] === "ENT" || lineWords[0] === "CAD" || lineWords[0] === "FLOT"){
-//       declarations.push(
-//         line.match(/#Ale[0-9]+/g).map(d => ({type: lineWords[0], variable : d}))
-//       )
-//     }
-
-//   }
-//   return declarations.flat()
-// }
-
-// function read() {
-//   //const lines = textarea.value.split("/[\s,\.]+/");
-//   const lines = textarea.value.split("\n");
-//   let words = [];
-//   for (let line of lines) {
-//     let lineWords = line.split(" ");
-
-//     words = words.concat(lineWords);
-
-//   }
-//   words.filter((word, index, self) => self.indexOf(word) === index);
-//   return words;
-// }
-
-// function removewords(declarations) {
-//   let array = read();
-//   return array.filter((word, index, self) => {
-//     return self.indexOf(word) === index
-//   })
-// }
-
-
-
 const compilador = document.getElementById('boton');
 const textarea = document.getElementById("textarea");
 const importbutton = document.getElementById("import")
 const regular_expresion = /#Ale[0-9]+/g
+const regex_ENT = /^3[0-9]+3$/g
+const regex_CAD = /(["'])[A-Z]+\1/gi
+const regex_FLOT = /[0-9]+\.3[0-9]+3/gi
+const regex_TYPE = /^(FLOT|CAD|ENT|return)$/
+const regex_separator = /[\s\{\}\(\)\,\;]/
+const regex_aritmetics= /^([+]|[-]|[\/]|[*]|[%]|[=])$/
+const regex_boolean = /^(&&|\|\|)$/
+const regex_letters = /[A-Z]/i
+
+
 const clean = document.getElementById("clean");
-
-
-
 
 
 document.getElementById('fileinput').addEventListener('change', readfile, false);
@@ -97,24 +32,25 @@ function readfile(e){
   }
   var lector = new FileReader();
   lector.onload = function(e) {
-    var contenido = e.target.result;
-    showText(contenido);
+    var content = e.target.result;
+    showText(content);
   };
   lector.readAsText(archivo);
 }
 
-function showText(contenido) {
+function showText(content) {
   var elemento = document.getElementById('textarea');
-  elemento.innerHTML = contenido;
+  elemento.innerHTML = content;
+  countlines();
 }
 
 
 
 function getLexema(){
+let contador = 0;
 let lines = textarea.value.split("\n").filter(line=>line!=" ")
-
 let lexemas = []
-
+for(let i = 0; i<lines.length;i++){
 for(let line of lines){
 const linelexemas = line.split(" ").filter(line=>line!=' ')
 
@@ -123,6 +59,9 @@ const linelexemas = line.split(" ").filter(line=>line!=' ')
       type: "",
       lexema
       }
+
+      console.log(Object.values(lexemaEntry))
+      
       switch(linelexemas[0]){
         case "ENT":
           if(lexema.match(regular_expresion)){
@@ -130,37 +69,147 @@ const linelexemas = line.split(" ").filter(line=>line!=' ')
               type: linelexemas[0],
               lexema
             }
+            lexemaExist = !!lexemas.find(lexema=>lexema.lexema == lexemaEntry.lexema)
+            if(!lexemaExist){
+              lexemas.push(lexemaEntry)
+            }
           }
         break;
-         
         case "CAD":
           if(lexema.match(regular_expresion)){
             lexemaEntry = {
               type: linelexemas[0],
               lexema
             }
+            lexemaExist = !!lexemas.find(lexema=>lexema.lexema == lexemaEntry.lexema)
+            if(!lexemaExist){
+              lexemas.push(lexemaEntry)
+            }
           }
         break;
-
         case "FLOT":
           if(lexema.match(regular_expresion)){
             lexemaEntry = {
               type: linelexemas[0],
               lexema
             }
+            lexemaExist = !!lexemas.find(lexema=>lexema.lexema == lexemaEntry.lexema)
+            if(!lexemaExist){
+              lexemas.push(lexemaEntry)
+            }
           }
         break;
       }
-     
 
-      let lexemaExist = !!lexemas.find(lexema=>lexema.lexema == lexemaEntry.lexema)
 
-      if(!lexemaExist){
-        lexemas.push(lexemaEntry)
+      
+      if(lexema.match(regex_ENT)){
+        lexemaEntry = {
+          type: "ENT",
+          lexema
+        }
+        lexemaExist = !!lexemas.find(lexema=>lexema.lexema == lexemaEntry.lexema)
+        if(!lexemaExist){
+          lexemas.push(lexemaEntry)
+        }
       }
+
+
+
+      if(lexema.match(regex_CAD)){
+        lexemaEntry = {
+          type: "CAD",
+          lexema
+        }
+        lexemaExist = !!lexemas.find(lexema=>lexema.lexema == lexemaEntry.lexema)
+        if(!lexemaExist){
+          lexemas.push(lexemaEntry)
+        }
+      }
+
+
+
+      if(lexema.match(regex_FLOT)){
+        lexemaEntry = {
+          type: "FLOT",
+          lexema
+        }
+        lexemaExist = !!lexemas.find(lexema=>lexema.lexema == lexemaEntry.lexema)
+        if(!lexemaExist){
+          lexemas.push(lexemaEntry)
+        }
+      }
+      if(lexema.match(regex_separator)){
+        lexemaEntry = {
+          type: " ",
+          lexema  
+        }
+
+         lexemaExist = !!lexemas.find(lexema=>lexema.lexema == lexemaEntry.lexema)
+        if(!lexemaExist){
+          lexemas.push(lexemaEntry)
+        }
+
+      }
+      if(lexema.match(regex_aritmetics)){
+        lexemaEntry = {
+          type: " ",
+          lexema  
+        }
+
+         lexemaExist = !!lexemas.find(lexema=>lexema.lexema == lexemaEntry.lexema)
+        if(!lexemaExist){
+          lexemas.push(lexemaEntry)
+        }
+
+      }
+
+
+      if(lexema.match(regex_boolean)){
+        lexemaEntry = {
+          type: " ",
+          lexema  
+        }
+
+         lexemaExist = !!lexemas.find(lexema=>lexema.lexema == lexemaEntry.lexema)
+        if(!lexemaExist){
+          lexemas.push(lexemaEntry)
+        }
+
+      }
+
+      if(lexema.match(regex_letters)){
+        lexemaEntry = {
+          type: " ",
+          lexema  
+        }
+
+         lexemaExist = !!lexemas.find(lexema=>lexema.lexema == lexemaEntry.lexema)
+        if(!lexemaExist){
+          lexemas.push(lexemaEntry)
+        }
+
+      }
+
+
+      if(lexema.match(regex_TYPE)){
+        lexemaEntry = {
+          type: " ",
+          lexema  
+        }
+
+         lexemaExist = !!lexemas.find(lexema=>lexema.lexema == lexemaEntry.lexema)
+        if(!lexemaExist){
+          lexemas.push(lexemaEntry)
+        }
+
+      }
+
+      contador++;
+    
     }
   }
-
+}
 return lexemas;
 
 }
@@ -185,3 +234,25 @@ clean.addEventListener("click", (e) => {
   // document.getElementById("table").innerHTML = "";
 
  })
+
+ const textarea1 = document.querySelector('#textarea');
+ const numerosLinea = document.querySelector('#numeros-linea');
+ 
+function countlines(){
+  const lineas = textarea.value.split('\n').length;
+  let numerosLineaHtml = '';
+  for (let i = 1; i <= lineas; i++) {
+    numerosLineaHtml += i + '<br>';
+  }
+  numerosLinea.innerHTML = numerosLineaHtml;
+}
+
+ textarea.addEventListener('input', function() {
+  countlines();
+ });
+
+
+ countlines();
+
+
+
